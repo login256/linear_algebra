447ad1d620bb9de6a1d4ca92d9d17f3a6d68a6d7
login256 <32592096+login256@users.noreply.github.com>
2017-10-31 19:37:34 +0800

    Add files via upload

---
+++ matrix.cpp
@@ -0,0 +1,157 @@
+#ifndef classmatrix
+#define classmatrix
+
+#include<algorithm>
+#include<iostream>
+#include<cstdio>
+#include"num.cpp"
+
+namespace linear_algebra
+{
+	using std::abs;
+	using std::swap;
+	const int MAXN=10;
+	class matrix
+	{
+		private:
+			num a[MAXN][MAXN];
+			int m,n;
+			bool b;
+		public:
+			matrix(){}
+			matrix(int mm,int nn,bool bb,num *aa):m(mm),n(nn),b(bb)
+			{
+				int cur=0;
+				for(int i=1;i<=m;i++)
+				{
+					for(int j=1;j<=n+b;j++)
+					{
+						a[i][j]=aa[cur];
+						cur++;
+					}
+				}
+			}
+			void print()
+			{
+				for(int j=1;j<=m;j++)
+				{
+					for(int k=1;k<=n+b;k++)
+					{
+						//printf("%8.4f",a[j][k]);
+						a[j][k].print(9);
+					}
+					putchar(10);
+				}
+			}
+			void guass(int need_print)	//need_print=0:不输出 1:输出操作 2:每次输出矩阵
+			{
+				int cur=0;
+				for(int i=1;i<=m;i++)
+				{
+					if(need_print)
+					{
+						printf("line %d:\n",i);
+					}
+					cur++;
+					while(cur<=n)
+					{
+						int j=i;
+						while(j<=m&&a[j][cur]==num(0))
+						{
+							j++;
+						}
+						if(j<=m)
+						{
+							if(i!=j)
+							{
+								for(int k=cur;k<=n+b;k++)
+								{
+									swap(a[i][k],a[j][k]);
+								}
+								if(need_print)
+								{
+									printf("(%d,%d), ",i,j);
+								}
+							}
+							break;
+						}
+						else
+						{
+							cur++;
+						}
+					}
+					if(cur>n)
+					{
+						break;
+					}
+					num lam{0};
+					for(int j=1;j<=m;j++)
+					{
+						if(i^j)
+						{
+							if(a[j][cur]!=0)
+							{
+								lam=-a[j][cur]/a[i][cur];
+								for(int k=cur;k<=n+b;k++)
+								{
+									a[j][k]=a[j][k]+(a[i][k]*lam);
+								}
+								if(need_print)
+								{
+									//printf("%.4lf(%d)+(%d), ",lam,i,j);
+									std::cout<<lam;
+									printf("(%d)+(%d), ",i,j);
+								}
+							}
+						}
+					}
+					if(need_print)
+					{
+						putchar(10);
+					}
+					if(need_print>=2)
+					{
+						print();
+					}
+				}
+				if(need_print)
+				{
+					std::cout<<"final:\n";
+				}
+				for(int i=1;i<=m;i++)
+				{
+					int be=0;
+					for(int j=1;j<=n;j++)
+					{
+						if(a[i][j]!=num(0))
+						{
+							be=j;
+							break;
+						}
+					}
+					if(be)
+					{
+						num lam=num(1)/a[i][be];
+						for(int j=be;j<=n+b;j++)
+						{
+							a[i][j]=a[i][j]*lam;
+						}
+						if(need_print)
+						{
+							//printf("%.4lf(%d), ",lam,i);
+							std::cout<<lam;
+							printf("(%d), ",i);
+						}
+					}
+				}
+				if(need_print)
+					putchar(10);
+				if(need_print>=2)
+				{
+					print();
+				}
+				return;
+			}
+	};
+}
+#endif
